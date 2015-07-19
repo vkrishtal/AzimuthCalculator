fc30e5328960f73f6f8949b7e6a68f5a285afffb
Vitaliy Krishtal <vkrishtal@yahoo.com>
2015-07-19 11:09:52 +0400

    Init commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AzimuthCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Ellipse_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_moveLine)
+            {
+                var pos = e.GetPosition(mainEllipse);
+
+                line.X2 = pos.X;
+                line.Y2 = pos.Y;
+            }
+        }
+
+        private bool _moveLine = true;
+
+        private void mouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                _moveLine = _moveLine ? false : true;
+            }
+        }
+
+    }
+}
